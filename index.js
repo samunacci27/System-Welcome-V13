8998009e3d6f1f1f6e3fc0a99a481cfe002218aa
StormSam <73138760+samunacci27@users.noreply.github.com>
2022-01-01 22:31:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+client.on("guildMemberAdd", (member) => {
+    client.channels.cache.get("").send(``) // < descrition
+  //                          /\
+  //      Id chat to send message Welcome
+})
+
+// ${member.toString()} TAG USER
+// ${member.guild.name} TAG Server
+// ${member.guild.memberCount} Member couter 
